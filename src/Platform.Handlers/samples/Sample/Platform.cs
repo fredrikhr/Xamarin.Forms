178bbe8eaefaacc75c395083c1c8564150827ff8
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2021-01-20 12:17:27 +0100

    Changes registering handlers

--- src/Platform.Handlers/samples/Sample/Platform.cs
+++ src/Platform.Handlers/samples/Sample/Platform.cs
@@ -14,12 +14,12 @@ namespace Sample
 				return;
 
 			HasInit = true;
-      
-      RegistrarHandlers.Handlers.Register<Button, ButtonHandler>();			
-      RegistrarHandlers.Handlers.Register<Label, LabelHandler>();
-      RegistrarHandlers.Handlers.Register<ProgressBar, ProgressBarHandler>();
+
+			RegistrarHandlers.Handlers.Register<Button, ButtonHandler>();
+			RegistrarHandlers.Handlers.Register<Label, LabelHandler>();
+			RegistrarHandlers.Handlers.Register<ProgressBar, ProgressBarHandler>();
 			RegistrarHandlers.Handlers.Register<Slider, SliderHandler>();
-      
+
 			RegistrarHandlers.Handlers.Register<VerticalStackLayout, LayoutHandler>();
 			RegistrarHandlers.Handlers.Register<HorizontalStackLayout, LayoutHandler>();
 			RegistrarHandlers.Handlers.Register<FlexLayout, LayoutHandler>();
